758fc8bcfe12fb72c45e1bcfd3f6f1aeaadecf34
ArpitAgarwal30 <arpit.agarwal67@gmail.com>
2020-10-11 22:53:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './bootstrap.css';
+import logo from './todo.png'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      newItem:"",
+      list:[]
+    }
+  }
+  addItem(todoValue){
+     if(todoValue!== ""){
+       const newItem = {
+         id:Date.now(),
+         value:todoValue,
+         isDone:false
+       }
+       const list = [...this.state.list];
+       list.push(newItem);
+       this.setState({
+         list:list,
+         newItem:""
+       })
+      }
+      // console.log(this.state.list);
+  }
+  updateInput(input){
+    // console.log(input);
+    this.setState({
+      newItem:input
+    })
+  }
+  deleteItem(id){
+    const list = [...this.state.list];
+    const updatedList = list.filter(item => item.id!== id);
+   this.setState({list:updatedList}) 
+  }
+  render(){
+    // console.log("hello");
+    return(
+      <div className="container">
+        <img src={logo} className= "img-thumbnail logo" alt="light" width="100" height="100"/>
+        <h1 className="text-center">Todo Items...</h1>
+        <div className="container">
+          <div className="form-group row">
+            <div className="col-sm-4 col-sm-push-8">
+              <input 
+              type="text" 
+              className="form-control" 
+              placeholder="Enter the Items" 
+              value={this.state.newItem}
+              onChange={e=>this.updateInput(e.target.value)}/>
+            </div>
+            <div className="col-sm-3">
+              <button onClick={()=>this.addItem(this.state.newItem)} className="btn btn-primary">Add Todo</button>
+            </div>
+            </div>
+            <div className="list">
+              <ul>
+                {this.state.list.map(item =>{
+                  return(
+                    <li>
+                      {item.value}
+                      <button className="btn" onClick={()=>this.deleteItem(item.id)}>Delete</button>
+                    </li>
+                  )
+                })}
+                </ul>  
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
-export default App;
+export default App
